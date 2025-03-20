44f56025cae4d26f707b9e0f5ebc25574ffe9377
amir.zaid <amir.zaid@silentmode.my>
2025-03-20 23:28:27 +0800

    chore(config): fetch cmb env

--- backend/internal/config/config.go
+++ backend/internal/config/config.go
@@ -5,41 +5,21 @@ import (
 )
 
 type Config struct {
-	LogLevel       string
-	WhatsappConfig WhatsappConfig
-	Database       DatabaseConfig
+	LogLevel        string
+	CallMeBotConfig CallMeBotConfig
 }
 
-type WhatsappConfig struct {
-	AccessToken   string
-	PhoneNumberID string
-}
-
-type DatabaseConfig struct {
-	Driver   string
-	Host     string
-	Port     string
-	User     string
-	Password string
-	Name     string
-	SSLMode  string
+type CallMeBotConfig struct {
+	PhoneNumber string
+	ApiKey      string
 }
 
 func Load() Config {
 	return Config{
 		LogLevel: getEnvWithDefault("LOG_LEVEL", "info"),
-		WhatsappConfig: WhatsappConfig{
-			AccessToken:   os.Getenv("WHATSAPP_ACCESS_TOKEN"),
-			PhoneNumberID: os.Getenv("WHATSAPP_PHONE_NUMBER_ID"),
-		},
-		Database: DatabaseConfig{
-			Driver:   getEnvWithDefault("DB_DRIVER", "mysql"),
-			Host:     getEnvWithDefault("DB_HOST", "127.0.0.1"),
-			Port:     getEnvWithDefault("DB_PORT", "3307"),
-			User:     getEnvWithDefault("DB_USER", "root"),
-			Password: getEnvWithDefault("DB_PASSWORD", "root"),
-			Name:     getEnvWithDefault("DB_NAME", "auto-messenger"),
-			SSLMode:  getEnvWithDefault("DB_SSLMODE", "disable"),
+		CallMeBotConfig: CallMeBotConfig{
+			PhoneNumber: os.Getenv("CMB_NUMBER"),
+			ApiKey:      os.Getenv("CMB_API_KEY"),
 		},
 	}
 }
